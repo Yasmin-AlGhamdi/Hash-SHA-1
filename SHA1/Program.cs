6d34223cecf2d7d63a150428646279f88e9a519a
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-27 14:41:19 +0300

    Add files via upload

---
+++ SHA1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA1
+{
+    class Program
+    {
+        static string Hash(string input)
+        {
+            //string source = "Hello World!";
+            using (SHA1Managed sha1Hash = new SHA1Managed()) //SHA1Managed sha1 = new SHA1Managed()
+            {
+                //From String to byte array
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha1Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+
+                Console.WriteLine("The SHA1 hash of " + input + " is: " + hash);
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            Hash("Do not use your bag");
+
+            Console.ReadKey();
+        }
+    }
+}
